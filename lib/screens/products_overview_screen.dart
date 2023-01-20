a37b389214c1f3d9990b9918f0b81476f130c68f
sukaran <sukarankant765@gmail.com>
2023-01-20 12:17:15 +0530

    add filters

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -4,18 +4,54 @@ import '../providers/product.dart';
 import '../widgets/product_item.dart';
 import '../providers/products.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
-
     // listeners
     final productsContainer = Provider.of<Products>(context);
-    final products = productsContainer.items;
+    final products = _showOnlyFavorites
+        ? productsContainer.favoriteItems
+        : productsContainer.items;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('EShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else if (selectedValue == FilterOptions.All) {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(10.0),
