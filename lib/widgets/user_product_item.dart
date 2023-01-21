799ed466b23de4a1f18aa3b0831297714cb75f48
sukaran <sukarankant765@gmail.com>
2023-01-21 11:29:16 +0530

    add user product screen

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  const UserProductItem({
+    Key key,
+    @required this.title,
+    @required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      title: Text(title),
+      // subtitle: Text(products[i].description),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {},
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {},
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
